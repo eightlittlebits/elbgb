114f7105c40afd43637d636fe1497d887b55bfd4
David Parrott <david.parrott@gmail.com>
2016-02-22 16:52:35 +0000

    Complete implementation of the 8-bit transfer opcodes and begin working on the 16-bit transfers

--- elbgb.gameboy/CPU/LR35902.cs
+++ elbgb.gameboy/CPU/LR35902.cs
@@ -20,6 +20,11 @@ namespace elbgb.gameboy.CPU
 		{
 			_mmu = mmu;
 		}
+
+		private void AddMachineCycles(int cycleCount)
+		{
+			_timestamp += (ulong)(4 * cycleCount);
+		}
 		
 		// Wrap the MMU ReadByte to handle the timing updates
 		private byte ReadByte(ushort address)
@@ -49,6 +54,20 @@ namespace elbgb.gameboy.CPU
 			_timestamp += 4;
 		}
 
+		// Push a byte on to the stack, decrement SP then write to memory
+		private void PushByte(byte value)
+		{
+			_r.SP--;
+			WriteByte(_r.SP, value);
+		}
+
+		// Push a word onto the stack, high byte pushed first
+		private void PushWord(ushort value)
+		{
+			PushByte((byte)(value >> 8));
+			PushByte((byte)value);
+		}
+
 		public void ExecuteSingleInstruction()
 		{
 			byte opcode = ReadByte(_r.PC++);
@@ -57,13 +76,139 @@ namespace elbgb.gameboy.CPU
 			{
 				case 0x00: return; // NOP
 
-				// Write A to (HL) and inc/dec
-				case 0x22: WriteByte(_r.HL++, _r.A); break; // LD (HL-),A
+				#region 8-bit transfer and input/output instructions
+
+				// load contents of register r' into register r
+				case 0x7F: _r.A = _r.A; break; // LD A,A
+				case 0x78: _r.A = _r.B; break; // LD A,B
+				case 0x79: _r.A = _r.C; break; // LD A,C
+				case 0x7A: _r.A = _r.D; break; // LD A,D
+				case 0x7B: _r.A = _r.E; break; // LD A,E
+				case 0x7C: _r.A = _r.H; break; // LD A,H
+				case 0x7D: _r.A = _r.L; break; // LD A,L
+				case 0x47: _r.B = _r.A; break; // LD B,A
+				case 0x40: _r.B = _r.B; break; // LD B,B
+				case 0x41: _r.B = _r.C; break; // LD B,C
+				case 0x42: _r.B = _r.D; break; // LD B,D
+				case 0x43: _r.B = _r.E; break; // LD B,E
+				case 0x44: _r.B = _r.H; break; // LD B,H
+				case 0x45: _r.B = _r.L; break; // LD B,L
+				case 0x4F: _r.C = _r.A; break; // LD C,A
+				case 0x48: _r.C = _r.B; break; // LD C,B
+				case 0x49: _r.C = _r.C; break; // LD C,C
+				case 0x4A: _r.C = _r.D; break; // LD C,D
+				case 0x4B: _r.C = _r.E; break; // LD C,E
+				case 0x4C: _r.C = _r.H; break; // LD C,H
+				case 0x4D: _r.C = _r.L; break; // LD C,L
+				case 0x57: _r.D = _r.A; break; // LD D,A
+				case 0x50: _r.D = _r.B; break; // LD D,B
+				case 0x51: _r.D = _r.C; break; // LD D,C
+				case 0x52: _r.D = _r.D; break; // LD D,D
+				case 0x53: _r.D = _r.E; break; // LD D,E
+				case 0x54: _r.D = _r.H; break; // LD D,H
+				case 0x55: _r.D = _r.L; break; // LD D,L
+				case 0x5F: _r.E = _r.A; break; // LD E,A
+				case 0x58: _r.E = _r.B; break; // LD E,B
+				case 0x59: _r.E = _r.C; break; // LD E,C
+				case 0x5A: _r.E = _r.D; break; // LD E,D
+				case 0x5B: _r.E = _r.E; break; // LD E,E
+				case 0x5C: _r.E = _r.H; break; // LD E,H
+				case 0x5D: _r.E = _r.L; break; // LD E,L
+				case 0x67: _r.H = _r.A; break; // LD H,A
+				case 0x60: _r.H = _r.B; break; // LD H,B
+				case 0x61: _r.H = _r.C; break; // LD H,C
+				case 0x62: _r.H = _r.D; break; // LD H,D
+				case 0x63: _r.H = _r.E; break; // LD H,E
+				case 0x64: _r.H = _r.H; break; // LD H,H
+				case 0x65: _r.H = _r.L; break; // LD H,L
+				case 0x6F: _r.L = _r.A; break; // LD L,A
+				case 0x68: _r.L = _r.B; break; // LD L,B
+				case 0x69: _r.L = _r.C; break; // LD L,C
+				case 0x6A: _r.L = _r.D; break; // LD L,D
+				case 0x6B: _r.L = _r.E; break; // LD L,E
+				case 0x6C: _r.L = _r.H; break; // LD L,H
+				case 0x6D: _r.L = _r.L; break; // LD L,L
+
+				// load 8-bit immediate data n into register r
+				case 0x3E: _r.A = ReadByte(_r.PC++); break; // LD A,n
+				case 0x06: _r.B = ReadByte(_r.PC++); break; // LD B,n
+				case 0x0E: _r.C = ReadByte(_r.PC++); break; // LD C,n
+				case 0x16: _r.D = ReadByte(_r.PC++); break; // LD D,n
+				case 0x1E: _r.E = ReadByte(_r.PC++); break; // LD E,n
+				case 0x26: _r.H = ReadByte(_r.PC++); break; // LD H,n
+				case 0x2E: _r.L = ReadByte(_r.PC++); break; // LD L,n
+
+				// load the contents of memory (8 bits) specified by register pair HL into register r
+				case 0x7E: _r.A = ReadByte(_r.HL); break; // LD A,(HL)
+				case 0x46: _r.B = ReadByte(_r.HL); break; // LD B,(HL)
+				case 0x4E: _r.C = ReadByte(_r.HL); break; // LD C,(HL)
+				case 0x56: _r.D = ReadByte(_r.HL); break; // LD D,(HL)
+				case 0x5E: _r.E = ReadByte(_r.HL); break; // LD E,(HL)
+				case 0x66: _r.H = ReadByte(_r.HL); break; // LD H,(HL)
+				case 0x6E: _r.L = ReadByte(_r.HL); break; // LD L,(HL)
+
+				// stores the content of register r in memory specififed by register pair HL
+				case 0x77: WriteByte(_r.HL, _r.A); break; // LD (HL),A
+				case 0x70: WriteByte(_r.HL, _r.B); break; // LD (HL),B
+				case 0x71: WriteByte(_r.HL, _r.C); break; // LD (HL),C
+				case 0x72: WriteByte(_r.HL, _r.D); break; // LD (HL),D
+				case 0x73: WriteByte(_r.HL, _r.E); break; // LD (HL),E
+				case 0x74: WriteByte(_r.HL, _r.H); break; // LD (HL),H
+				case 0x75: WriteByte(_r.HL, _r.L); break; // LD (HL),L
+
+				// loads 8-bit immediate data n into memory specified by register pair HL
+				case 0x36: WriteByte(_r.HL, ReadByte(_r.PC++)); break; // LD (HL),n
+
+				// load the contents of memory (8 bits) specified by register pair into register A
+				case 0x0A: _r.A = ReadByte(_r.BC); break; // LD A,(BC)
+				case 0x1A: _r.A = ReadByte(_r.DE); break; // LD A,(DE)
+
+				// load/store high memory, indexed on C
+				case 0xF2: _r.A = ReadByte((ushort)(0xFF00 + _r.C)); break; // LD A,($FF00+C)
+				case 0xE2: WriteByte((ushort)(0xFF00 + _r.C), _r.A); break; // LD ($FF00+C),A
+
+				// load/store high memory, indexed on immediate
+				case 0xF0: _r.A = ReadByte((ushort)(0xFF00 + ReadByte(_r.PC++))); break; // LD A,($FF00+n)
+				case 0xE0: WriteByte((ushort)(0xFF00 + ReadByte(_r.PC++)), _r.A); break; // LD ($FF00+n),A
+
+				// load/store register A from/to internal RAM or register specified by 16-bit immediate
+				case 0xFA: _r.A = ReadByte(ReadWord(_r.PC)); _r.PC += 2; break; // LD A,(nn)
+				case 0xEA: WriteByte(ReadWord(_r.PC), _r.A); _r.PC += 2; break; // LD (nn),A
+
+				// load the contents of memory (8 bits) specified by register pair HL into register A
+				// and simultaneously increment/decrement HL
+				case 0x2A: _r.A = ReadByte(_r.HL++); break; // LD A,(HL+)
+				case 0x3A: _r.A = ReadByte(_r.HL--); break; // LD A,(HL-)
+
+				// Store the contents of register A in the memory specified by register pair 
+				case 0x02: WriteByte(_r.BC, _r.A); break; // LD (BC),A
+				case 0x12: WriteByte(_r.DE, _r.A); break; // LD (DE),A
+
+				// store the contents of register A in the memoryspecified by register pair HL
+				// and simultaneously increment/decrement HL
+				case 0x22: WriteByte(_r.HL++, _r.A); break; // LD (HL+),A
 				case 0x32: WriteByte(_r.HL--, _r.A); break; // LD (HL-),A
 
-				// Load 16 bit immediate
+				#endregion
+
+				#region 16-bit transfer instructions
+
+				// load 2 bytes of immediate data to register pair
+				case 0x01: _r.BC = ReadWord(_r.PC); _r.PC += 2; break; // LD BC,nn
+				case 0x11: _r.DE = ReadWord(_r.PC); _r.PC += 2; break; // LD DE,nn
 				case 0x21: _r.HL = ReadWord(_r.PC); _r.PC += 2; break; // LD HL,nn
 				case 0x31: _r.SP = ReadWord(_r.PC); _r.PC += 2; break; // LD SP,nn
+
+				// load the contents of register pair HL in stack pointer SP
+				case 0xF9: _r.SP = _r.HL; AddMachineCycles(1); break; // LD SP,HL
+
+				// push contents of register pair onto the stack
+				case 0xC5: AddMachineCycles(1); PushWord(_r.BC); break; // PUSH BC
+				case 0xD5: AddMachineCycles(1); PushWord(_r.DE); break; // PUSH DE
+				case 0xE5: AddMachineCycles(1); PushWord(_r.HL); break; // PUSH HL
+				case 0xF5: AddMachineCycles(1); PushWord(_r.AF); break; // PUSH AF
+
+				#endregion
 
 				// 8 bit ALU
 				case 0xAF: _r.A = Xor8Bit(_r.A, _r.A); break;				// XOR A
@@ -114,7 +259,7 @@ namespace elbgb.gameboy.CPU
 
 		private void Bit(byte reg, int bit)
 		{
-			// carry flag unaffected, preserve state
+			// carry flag unaffected, preserve state and reset others
 			_r.F &= Registers.Flags.C;
 
 			// half carry flag set
