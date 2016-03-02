4598432aa482c22656c62838117cea95d84c054a
David Parrott <david.parrott@gmail.com>
2016-03-02 20:22:44 +0000

    Update memory handling, add work and high ram

--- elbgb.gameboy/Memory/MMU.cs
+++ elbgb.gameboy/Memory/MMU.cs
@@ -7,12 +7,20 @@ using elbgb.gameboy.Display;
 
 namespace elbgb.gameboy.Memory
 {
-	class MMU : IMemoryMappedComponent
+	class MMU //: IMemoryMappedComponent
 	{
+		public static class Registers
+		{
+			public const ushort BOOTROMLOCK = 0xFF50;
+		}
+
 		private GameBoy _gb;
 
 		private bool _bootRomLocked;
 		private byte[] _bootRom;
+
+		private byte[] _wram;
+		private byte[] _hram;
 
 		public MMU(GameBoy gameBoy, byte[] bootRom)
 		{
@@ -20,41 +28,99 @@ namespace elbgb.gameboy.Memory
 
 			_bootRom = bootRom;
 			_bootRomLocked = false;
+
+			_wram = new byte[0x2000];
+			_hram = new byte[0x7F];
 		}
 
 		public byte ReadByte(ushort address)
 		{
-			if (!_bootRomLocked && address < 0x100)
+			switch (address & 0xF000)
 			{
-				return _bootRom[address];
-			}
-			// timer 0xFF04 - 0xFF07
-			else if (address >= 0xFF04 && address <= 0xFF07)
-			{
-				return _gb.Timer.ReadByte(address);
-			}
+				case 0x0000:
+					if (!_bootRomLocked && address < 0x100)
+					{
+						return _bootRom[address];
+					}
 
-			throw new NotImplementedException();
+					throw new NotImplementedException();
+
+				// working ram
+				case 0xC000:
+				case 0xD000:
+					return _wram[address & 0x1FFF];
+
+				case 0xF000:
+					// timer IO registers
+					if (address >= 0xFF04 && address <= 0xFF07)
+					{
+						return _gb.Timer.ReadByte(address);
+					}
+					// restricted area
+					else if (address >= 0xFEA0 && address <= 0xFEFF)
+					{
+						return 0;
+					}
+					// hi ram
+					else if (address >= 0xFF80 && address <= 0xFFFE)
+					{
+						return _hram[address & 0x7F];
+					}
+					else
+						throw new NotImplementedException();
+
+				default:
+					throw new NotImplementedException();
+			}
 		}
 
 		public void WriteByte(ushort address, byte value)
 		{
-			if (address == 0xFF50) // Boot ROM lockout address
+			switch (address & 0xF000)
 			{
-				// TODO(david): do we set a value at this location? can we read from it?
-				_bootRomLocked = true;
+				// working ram
+				case 0xC000:
+				case 0xD000:
+					_wram[address & 0x1FFF] = value;
+					return;
+
+				// vram
+				case 0x8000:
+				case 0x9000:
+					_gb.PPU.WriteByte(address, value);
+					return;
+
+				case 0xF000:
+					// timer IO registers
+					if (address >= 0xFF04 && address <= 0xFF07)
+					{
+						_gb.Timer.WriteByte(address, value);
+						return;
+					}
+					// restricted area
+					else if (address >= 0xFEA0 && address <= 0xFEFF)
+					{
+						return;
+					}
+					// hi ram
+					else if (address >= 0xFF80 && address <= 0xFFFE)
+					{
+						_hram[address & 0x7F] = value;
+						return;
+					}
+					else if (address == Registers.BOOTROMLOCK)
+					{
+						// TODO(david): do we set a value at this location? can we read from it?
+						_bootRomLocked = true;
+						return;
+					}
+					else
+						throw new NotImplementedException();
+
+
+				default:
+					throw new NotImplementedException();
 			}
-			else if (address >= 0x8000 && address <= 0x9fff)
-			{
-				_gb.PPU.WriteByte(address, value);
-			}
-			// timer 0xFF04 - 0xFF07
-			else if (address >= 0xFF04 && address <= 0xFF07)
-			{
-				_gb.Timer.WriteByte(address, value);
-			}
-			else
-				throw new NotImplementedException();
 		}
 	}
 }
