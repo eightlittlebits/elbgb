92347692d45ecd52e1698d7fac01a3cdcd612983
David Parrott <david.parrott@gmail.com>
2016-03-09 11:40:45 +0000

    Implement timer functionality

--- elbgb.gameboy/Timer.cs
+++ elbgb.gameboy/Timer.cs
@@ -16,8 +16,17 @@ namespace elbgb.gameboy
 			public const ushort TAC  = 0xFF07;
 		}
 
-		private ushort _div;
+		// divider
+		private ushort _divider;
 
+		// timer
+		private bool _timerEnabled;
+
+		private uint _timerInterval;
+		private uint _timerCounter;
+
+		private byte _tima, _tma, _tac;
+		
 		public Timer(GameBoy gameBoy)
 			: base(gameBoy)
 		{
@@ -30,7 +39,21 @@ namespace elbgb.gameboy
 
 			switch (address)
 			{
-				case Registers.DIV: return (byte)(_div >> 8);
+				// the divider is the upper 8 bits of the 16-bit counter that counts the 
+				// basic clock frequency (f). f = 4.194304 MHz
+				case Registers.DIV: return (byte)(_divider >> 8);
+
+				// timer counter
+				case Registers.TIMA:
+					return _tima;
+
+				// timer modulo 
+				case Registers.TMA:
+					return _tma;
+
+				// timer controller
+				case Registers.TAC:
+					return (byte)(_tac & 0x07);
 
 				default:
 					throw new ArgumentOutOfRangeException("address");
@@ -44,7 +67,39 @@ namespace elbgb.gameboy
 			switch (address)
 			{
 				// a write always clears the upper 8 bits of DIV, regardless of value
-				case Registers.DIV: _div &= 0x00FF; break;
+				case Registers.DIV: _divider &= 0x00FF; break;
+
+				// timer counter
+				case Registers.TIMA:
+					_tima = value; break;
+
+				// timer modulo 
+				case Registers.TMA:
+					_tma = value; break;
+
+				// timer controller
+				case Registers.TAC:
+					_tac = (byte)(value & 0x07);
+
+					// update enabled state from bit 2
+					_timerEnabled = (value & 0x04) == 0x04;
+
+					// bits 0/1 specify timer frequency, set interval
+					switch (value & 0x03)
+					{
+						// 00 - f/2^10 (4.096 KHz)
+						case 0x00: _timerInterval = SystemClock.ClockFrequency / 1024; break;
+
+						// 01 - f/2^4 (262.144 KHz)
+						case 0x01: _timerInterval = SystemClock.ClockFrequency / 16; break;
+
+						// 10 - f/2^6 (65.436 KHz)
+						case 0x10: _timerInterval = SystemClock.ClockFrequency / 64; break;
+
+						// 11 - f/2^8 (16.384 KHz)
+						case 0x11: _timerInterval = SystemClock.ClockFrequency / 256; break;
+					}
+					break;
 
 				default:
 					throw new ArgumentOutOfRangeException("address");
@@ -54,11 +109,33 @@ namespace elbgb.gameboy
 		public override void Update(ulong cycleCount)
 		{
 			UpdateDivider(cycleCount);
+			UpdateTimer(cycleCount);
 		}
 
 		private void UpdateDivider(ulong cycleCount)
 		{
-			_div += (ushort)cycleCount;
+			_divider += (ushort)cycleCount;
+		}
+
+		private void UpdateTimer(ulong cycleCount)
+		{
+			if (_timerEnabled)
+			{
+				_timerCounter += (uint)cycleCount;
+
+				while (_timerCounter >= _timerInterval)
+				{
+					_tima++;
+					_timerCounter -= _timerInterval;
+
+					if (_tima == 0)
+					{
+						_tima = _tma;
+
+						// TODO(david): request timer overflow interrupt
+					}
+				}
+			}
 		}
 	}
 }
