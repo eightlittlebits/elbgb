dada7a878ef6b6d81382c8a384f916bc454dd67b
David Parrott <david.parrott@gmail.com>
2016-03-10 16:51:34 +0000

    PPU work, implement LCDC register

--- elbgb.gameboy/Display/PPU.cs
+++ elbgb.gameboy/Display/PPU.cs
@@ -9,22 +9,39 @@ namespace elbgb.gameboy.Display
 	{
 		public static class Registers
 		{
-			public const ushort LCDC = 0xFF40;
-			public const ushort STAT = 0xFF41;
-			public const ushort SCY	 = 0xFF42;
-			public const ushort SCX	 = 0xFF43;
-			public const ushort LY   = 0xFF44;
-			public const ushort LYC	 = 0xFF45;
-			public const ushort DMA	 = 0xFF46;
+			// LCD display registers
+			public const ushort LCDC = 0xFF40; // LCD control
+			public const ushort STAT = 0xFF41; // LCD status
+			public const ushort SCY	 = 0xFF42; // scroll y
+			public const ushort SCX	 = 0xFF43; // scroll x
+			public const ushort LY   = 0xFF44; // LCD y co-ord
+			public const ushort LYC	 = 0xFF45; // LCD y compare
+
+			// DMA register
+			public const ushort DMA	 = 0xFF46; 
+			
+			// palette data
 			public const ushort BGP	 = 0xFF47;
 			public const ushort OBP0 = 0xFF48;
 			public const ushort OBP1 = 0xFF49;
-			public const ushort WY	 = 0xFF4A;
-			public const ushort WX   = 0xFF4B;
+			
+			// LCD display registers
+			public const ushort WY	 = 0xFF4A; // window y
+			public const ushort WX   = 0xFF4B; // window x
 		}
 
 		private byte[] _vram;
 		private byte[] _oam;
+
+		private byte _lcdControl;
+		private bool _displayEnabled;				// LCDC bit 7 - 0: Off / 1: On
+		private ushort _windowTileBaseAddress;		// LCDC bit 6 - 0: 0x9800 - 0x9Bff / 1 : 0x9C00 = 0x9FFF
+		private bool _windowEnabled;				// LCDC bit 5 - 0: Off / 1: On
+		private ushort _backgroundCharBaseAddress;	// LCDC bit 4 - 0: 0x8800 - 0x97ff / 1 : 0x8000 = 0x8FFF
+		private ushort _backgroundTileBaseAddress;	// LCDC bit 3 - 0: 0x9800 - 0x9Bff / 1 : 0x9C00 = 0x9FFF
+		private int _spriteHeight;					// LCDC bit 2 - 0: 8x8 / 1: 8x16 (w*h)
+		private bool _spritesEnabled;				// LCDC bit 1 - 0: Off / 1: On
+		private bool _backgroundEnabled;			// LCDC bit 0 - 0: Off / 1: On
 
 		private byte _scrollY, _scrollX;
 
@@ -45,6 +62,9 @@ namespace elbgb.gameboy.Display
 
 			switch (address)
 			{
+				case Registers.LCDC:
+					return _lcdControl;
+
 				case Registers.SCY:
 					return _scrollY;
 
@@ -74,6 +94,20 @@ namespace elbgb.gameboy.Display
 			{
 				switch (address)
 				{
+					case Registers.LCDC:
+						_lcdControl = value;
+
+						_displayEnabled = (_lcdControl & 0x80) == 0x80;
+						_windowTileBaseAddress = (ushort)((_lcdControl & 0x40) == 0x40 ? 0x9C00 : 0x9800);
+						_windowEnabled = (_lcdControl & 0x20) == 0x20;
+						_backgroundCharBaseAddress = (ushort)((_lcdControl & 0x10) == 0x10 ? 0x8000 : 0x8800);
+						_backgroundTileBaseAddress = (ushort)((_lcdControl & 0x08) == 0x08 ? 0x9C00 : 0x9800);
+						_spriteHeight = (_lcdControl & 0x04) == 0x04 ? 16 : 8; 
+						_spritesEnabled = (_lcdControl & 0x02) == 0x02;
+						_backgroundEnabled = (_lcdControl & 0x01) == 0x01;
+
+						break;
+
 					case Registers.SCY:
 						_scrollY = value; break;
 
