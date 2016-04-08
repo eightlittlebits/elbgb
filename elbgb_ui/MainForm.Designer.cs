7d7cebf8ce3b30a291e5c7067778aa733f73f352
David Parrott <david.parrott@gmail.com>
2016-04-08 16:54:15 +0100

    Add ui project, first experiments in getting something displayed on screen

---
+++ elbgb_ui/MainForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace elbgb_ui
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.regenerateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.displayPanel = new System.Windows.Forms.Panel();
+			this.imageTypeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.bppIndexedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.bppARGBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.imageTypeToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(172, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.regenerateToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "&File";
+			// 
+			// regenerateToolStripMenuItem
+			// 
+			this.regenerateToolStripMenuItem.Name = "regenerateToolStripMenuItem";
+			this.regenerateToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.regenerateToolStripMenuItem.Text = "Regenerate";
+			this.regenerateToolStripMenuItem.Click += new System.EventHandler(this.regenerateToolStripMenuItem_Click);
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.exitToolStripMenuItem.Text = "E&xit";
+			// 
+			// displayPanel
+			// 
+			this.displayPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.displayPanel.Location = new System.Drawing.Point(0, 24);
+			this.displayPanel.Name = "displayPanel";
+			this.displayPanel.Size = new System.Drawing.Size(172, 168);
+			this.displayPanel.TabIndex = 2;
+			this.displayPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.displayPanel_Paint);
+			// 
+			// imageTypeToolStripMenuItem
+			// 
+			this.imageTypeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bppIndexedToolStripMenuItem,
+            this.bppARGBToolStripMenuItem});
+			this.imageTypeToolStripMenuItem.Name = "imageTypeToolStripMenuItem";
+			this.imageTypeToolStripMenuItem.Size = new System.Drawing.Size(78, 20);
+			this.imageTypeToolStripMenuItem.Text = "ImageType";
+			// 
+			// bppIndexedToolStripMenuItem
+			// 
+			this.bppIndexedToolStripMenuItem.Name = "bppIndexedToolStripMenuItem";
+			this.bppIndexedToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.bppIndexedToolStripMenuItem.Text = "4bpp Indexed";
+			this.bppIndexedToolStripMenuItem.Click += new System.EventHandler(this.bppIndexedToolStripMenuItem_Click);
+			// 
+			// bppARGBToolStripMenuItem
+			// 
+			this.bppARGBToolStripMenuItem.Name = "bppARGBToolStripMenuItem";
+			this.bppARGBToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.bppARGBToolStripMenuItem.Text = "32bpp ARGB";
+			this.bppARGBToolStripMenuItem.Click += new System.EventHandler(this.bppARGBToolStripMenuItem_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(172, 192);
+			this.Controls.Add(this.displayPanel);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "MainForm";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+		private System.Windows.Forms.Panel displayPanel;
+		private System.Windows.Forms.ToolStripMenuItem regenerateToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem imageTypeToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem bppIndexedToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem bppARGBToolStripMenuItem;
+	}
+}
+
---
+++ elbgb_ui/MainForm.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace elbgb_ui
+{
+	public partial class MainForm : Form
+	{
+		private Func<Bitmap> _imageGenerateMethod;
+
+		private Bitmap _displayBuffer;
+
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			int width = 160 * 2;
+			int height = 144 * 2 + menuStrip1.Height;
+
+			this.ClientSize = new Size(width, height);
+
+			_imageGenerateMethod = Generate4bppIndexedImage;
+			bppIndexedToolStripMenuItem.Checked = true;
+
+			_displayBuffer = _imageGenerateMethod();
+		}
+
+		private Bitmap Generate4bppIndexedImage()
+		{
+			var rand = new Random();
+
+			Bitmap image = new Bitmap(160, 144, PixelFormat.Format4bppIndexed);
+
+			// set up our 2 bit palette
+			var palette = image.Palette;
+
+			palette.Entries[0] = Color.FromArgb(255, 255, 255, 255);
+			palette.Entries[1] = Color.FromArgb(255, 183, 183, 183);
+			palette.Entries[2] = Color.FromArgb(255, 104, 104, 104);
+			palette.Entries[3] = Color.FromArgb(255, 0, 0, 0);
+
+			image.Palette = palette;
+
+			// fill bitmap with random palette entries 0-3
+			var imageData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height),
+												ImageLockMode.WriteOnly,
+												image.PixelFormat);
+
+			unsafe
+			{
+
+				for (int y = 0; y < imageData.Height; y++)
+				{
+					byte* row = (byte*)imageData.Scan0 + (y * imageData.Stride);
+
+					for (int x = 0; x < imageData.Width / 2; x++)
+					{
+						int pixelA = rand.Next(4);
+						int pixelB = rand.Next(4);
+
+						byte indexedPixel = (byte)((pixelA << 4) | pixelB);
+
+						row[x] = indexedPixel;
+					}
+				}
+			}
+
+			image.UnlockBits(imageData);
+
+			return image;
+		}
+
+		private Bitmap Generate32bppArgbImage()
+		{
+			var rand = new Random();
+
+			Bitmap image = new Bitmap(160, 144, PixelFormat.Format32bppArgb);
+
+			// fill bitmap with random colours
+			var imageData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height),
+												ImageLockMode.WriteOnly,
+												image.PixelFormat);
+
+			var pixelFormatSize = Image.GetPixelFormatSize(image.PixelFormat) / 8;
+
+			unsafe
+			{
+				for (int y = 0; y < imageData.Height; y++)
+				{
+					byte* row = (byte*)imageData.Scan0 + (y * imageData.Stride);
+
+					for (int x = 0; x < imageData.Width; x++)
+					{
+						byte* pixel = row + (x * pixelFormatSize);
+
+						pixel[0] = (byte)rand.Next(256); // B
+						pixel[1] = (byte)rand.Next(256); // G
+						pixel[2] = (byte)rand.Next(256); // R
+						pixel[3] = (byte)rand.Next(256); // A
+					}
+				}
+			}
+
+			image.UnlockBits(imageData);
+
+			return image;
+		}
+
+		private void displayPanel_Paint(object sender, PaintEventArgs e)
+		{
+			e.Graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.Half;
+			e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+			e.Graphics.DrawImage(_displayBuffer, 0, 0, displayPanel.Width, displayPanel.Height);
+		}
+
+		private void regenerateToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			_displayBuffer = _imageGenerateMethod();
+			displayPanel.Invalidate();
+		}
+
+		private void bppIndexedToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			_imageGenerateMethod = Generate4bppIndexedImage;
+			bppIndexedToolStripMenuItem.Checked = true;
+			bppARGBToolStripMenuItem.Checked = false;
+
+			_displayBuffer = _imageGenerateMethod();
+			displayPanel.Invalidate();
+		}
+
+		private void bppARGBToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			_imageGenerateMethod = Generate32bppArgbImage;
+			bppIndexedToolStripMenuItem.Checked = false;
+			bppARGBToolStripMenuItem.Checked = true;
+
+			_displayBuffer = _imageGenerateMethod();
+			displayPanel.Invalidate();
+		}
+	}
+}
