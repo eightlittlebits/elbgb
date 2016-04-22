5d344bc818c8149c6cb80f317329579f1991d01c
David Parrott <david.parrott@gmail.com>
2016-04-22 15:10:32 +0100

    Explain the unusual control styles in DoubleBufferedPanel.

--- elbgb_ui/Components/DoubleBufferedPanel.cs
+++ elbgb_ui/Components/DoubleBufferedPanel.cs
@@ -11,9 +11,16 @@ namespace elbgb_ui.Components
 	{
 		public DoubleBufferedPanel()
 		{
-			this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
 			this.SetStyle(ControlStyles.ResizeRedraw, true);
 			this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+			// these settings as applied below conflict with the documents,
+			// we're ok in this instance though as we're rendering the control
+			// ourselves constantly. We don't need windows to be raising either
+			// the WM_ERASEBKGND or WM_PAINT events. Seetting 
+			// AllPaintingInWmPaint to true prevents WM_ERASEBKGND and setting
+			// UserPaint to false prevents WM_PAINT
+			this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
 			this.SetStyle(ControlStyles.UserPaint, false);
 
 			this.UpdateStyles();
