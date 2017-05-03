7387db708710f4997eaff49541ecf8d12bf55125
David Parrott <david.parrott@gmail.com>
2017-05-03 11:07:12 +0100

    Move setting of OptimizedDoubleBuffer style in DoubleBufferedPanel
    The docs state that the OptimizedDoubleBuffer should be used in conjunction with the AllPaintingInWmPaint style

--- elbgb_ui/Components/DoubleBufferedPanel.cs
+++ elbgb_ui/Components/DoubleBufferedPanel.cs
@@ -7,45 +7,47 @@ using System.Windows.Forms;
 
 namespace elbgb_ui.Components
 {
-	class DoubleBufferedPanel : Panel
-	{
-		private bool _realTimeUpdate;
+    class DoubleBufferedPanel : Panel
+    {
+        private bool _realTimeUpdate;
 
-		public DoubleBufferedPanel()
-		{
-			this.SetStyle(ControlStyles.ResizeRedraw, true);
-			this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
-		}
+        public DoubleBufferedPanel()
+        {
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+        }
 
-		public bool RealTimeUpdate
-		{
-			get
-			{
-				return _realTimeUpdate;
-			}
-			set
-			{
-				_realTimeUpdate = value;
+        public bool RealTimeUpdate
+        {
+            get
+            {
+                return _realTimeUpdate;
+            }
+            set
+            {
+                _realTimeUpdate = value;
 
-				if (value == true)
-				{
-					// these settings as applied below conflict with the documents,
-					// we're ok in this instance though as we're rendering the control
-					// ourselves constantly. We don't need windows to be raising either
-					// the WM_ERASEBKGND or WM_PAINT events. Seetting 
-					// AllPaintingInWmPaint to true prevents WM_ERASEBKGND and setting
-					// UserPaint to false prevents WM_PAINT
-					this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
-					this.SetStyle(ControlStyles.UserPaint, false);
-				}
-				else
-				{
-					this.SetStyle(ControlStyles.AllPaintingInWmPaint, false);
-					this.SetStyle(ControlStyles.UserPaint, false);
-				}
+                if (value == true)
+                {
+                    this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
 
-				this.UpdateStyles();
-			}
-		}
-	}
+                    // these settings as applied below conflict with the documents,
+                    // we're ok in this instance though as we're rendering the control
+                    // ourselves constantly. We don't need windows to be raising either
+                    // the WM_ERASEBKGND or WM_PAINT events. Seetting 
+                    // AllPaintingInWmPaint to true prevents WM_ERASEBKGND and setting
+                    // UserPaint to false prevents WM_PAINT
+                    this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+                    this.SetStyle(ControlStyles.UserPaint, false);
+                }
+                else
+                {
+                    this.SetStyle(ControlStyles.OptimizedDoubleBuffer, false);
+                    this.SetStyle(ControlStyles.AllPaintingInWmPaint, false);
+                    this.SetStyle(ControlStyles.UserPaint, false);
+                }
+
+                this.UpdateStyles();
+            }
+        }
+    }
 }
