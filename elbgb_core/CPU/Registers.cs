f5fbfe452f36c5b70aa2f2ac3ef2725249cac271
David Parrott <david.parrott@gmail.com>
2019-12-17 11:08:48 +0000

    Adjust register layout if building on big endian

--- elbgb_core/CPU/Registers.cs
+++ elbgb_core/CPU/Registers.cs
@@ -3,6 +3,27 @@
 namespace elbgb_core.CPU
 {
     [StructLayout(LayoutKind.Explicit)]
+    internal
+#if BIG_ENDIAN
+    struct Registers
+    {
+        [FieldOffset(0)] public ushort AF;
+        [FieldOffset(0)] public byte A;
+        [FieldOffset(1)] public StatusFlags F;
+
+        [FieldOffset(2)] public ushort BC;
+        [FieldOffset(2)] public byte B;
+        [FieldOffset(3)] public byte C;
+
+        [FieldOffset(4)] public ushort DE;
+        [FieldOffset(4)] public byte D;
+        [FieldOffset(5)] public byte E;
+
+        [FieldOffset(6)] public ushort HL;
+        [FieldOffset(6)] public byte H;
+        [FieldOffset(7)] public byte L;
+    }
+#else
     struct Registers
     {
         [FieldOffset(0)] public ushort AF;
@@ -21,4 +42,5 @@ namespace elbgb_core.CPU
         [FieldOffset(7)] public byte H;
         [FieldOffset(6)] public byte L;
     }
+#endif
 }
